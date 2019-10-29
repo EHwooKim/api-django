a4d60c7abfe048caa1df9b4e736fc07cfae6ab92
EHwooKim <ehwoo0707@naver.com>
2019-10-29 10:24:34 +0900

    Music index, detail

---
+++ musics/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+
+app_name='musics'
+
+urlpatterns = [
+    path('musics/', views.index, name='index'),
+    path('musics/<int:music_pk>/', views.detail, name='datail'),
+    
+]
--- musics/views.py
+++ musics/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Music
+from .serializers import MusicSerializers
 # Create your views here.
+
+
+@api_view(['GET']) # HTTP method의 GET요청만. POST일떄는 결과 안나온다.
+def index(request):
+    musics = Music.objects.all()
+    # 여기서 return render했었지만 우리는 지금 api하는거니까
+    serializer = MusicSerializers(musics, many=True)  # 이거는 QuertSet이라 many=Ture한거고
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def detail(request, music_pk):
+    music = get_object_or_404(Music, pk=music_pk)     
+    serializer = MusicSerializers(music)             # 이거는 하나 받는거라 many 옵션 필요없다.
+    return Response(serializer.data)
